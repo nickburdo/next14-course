64587f3133e6b0200383aec34ba5754e03025b8c
Mykola Burdo <mykola.burdo@altamira.ai>
2024-06-14 17:43:59 +0300

    Install prettier

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-blue-900">
+      <div className="container m-auto p-8 text-center">
+        Created by &copy; Nick Burdo {new Date().getFullYear()}
+      </div>
+    </footer>
+  );
+}

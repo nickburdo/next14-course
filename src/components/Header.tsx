79a760a0ae405353428f0a3443ea309450fcc8ef
Mykola Burdo <mykola.burdo@altamira.ai>
2024-06-15 13:23:04 +0300

    Main Menu refactoring

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,22 @@
 import MainMenu from "@/components/MainMenu";
 
+export interface MenuItem {
+  id: number;
+  name: string;
+  path: string;
+}
+
 export default function Header() {
+  const mainMenu: MenuItem[] = [
+    { id: 1, name: "Home", path: "/" },
+    { id: 2, name: "Blog", path: "/blog" },
+    { id: 3, name: "About", path: "/about" },
+  ];
+
   return (
     <header className="bg-blue-900 text-white">
       <div className="container my-auto p-8">
-        <MainMenu />
+        <MainMenu mainMenu={mainMenu} />
       </div>
     </header>
   );
--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,14 +1,13 @@
 "use client";
+import { MenuItem } from "@/components/Header";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const mainMenu = [
-  { id: 1, name: "Home", path: "/" },
-  { id: 2, name: "Blog", path: "/blog" },
-  { id: 3, name: "About", path: "/about" },
-];
+interface Props {
+  mainMenu: MenuItem[];
+}
 
-export default function MainMenu() {
+export default function MainMenu({ mainMenu }: Props) {
   const pathname = usePathname();
   const isActive = (path: string) =>
     path === "/" ? path === pathname : pathname.startsWith(path);
